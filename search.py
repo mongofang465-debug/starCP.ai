654fdd3b5ccd14715dcb9e37184757d6227e2dd0
mongofang465-debug <mongofang465@gmail.com>
2026-06-05 05:29:36 +0800

    Update search.py

--- search.py
+++ search.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 client = OpenAI(
-    api_key=os.getenv("OPENROUTER_API_KEY"),
+    api_key=st.secrets("OPENROUTER_API_KEY"),
     base_url="https://openrouter.ai/api/v1"
 )
 
