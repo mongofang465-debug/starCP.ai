35fca2b86ebf916cf03bbf4c5e56c1ce6d4431cf
mongofang465-debug <mongofang465@gmail.com>
2026-06-05 05:29:22 +0800

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # OpenRouter Key
 client = OpenAI(
-    api_key=os.getenv("OPENROUTER_API_KEY"),
+    api_key=st.secrets("OPENROUTER_API_KEY"),
     base_url="https://openrouter.ai/api/v1"
 )
 
